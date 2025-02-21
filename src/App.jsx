6adc679948aa705a16d58b232b554db5e416ce13
rahul prajapati <rahulprajapati2930@gmail.com>
2025-02-21 16:15:06 +0530

    modifies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Routes , Route} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import About from './Components/About'
 import OurMenu from './Components/OurMenu'
@@ -10,20 +10,20 @@ import LoginRegister from './Pages/Home/LoginRegister'
 import Scroll from './Components/Scroll'
 
 function App() {
-  
+
 
   return (
     <>
-      <BrowserRouter basename="/Burger_deliveryApp_UI/">
-      <Scroll/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/menu' element={<OurMenu/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/Login' element={<LoginRegister/>}/>
-      </Routes>
-      
+      <BrowserRouter basename='/Burger_deliveryApp_UI/'>
+        <Scroll />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/menu' element={<OurMenu />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/Login' element={<LoginRegister />} />
+        </Routes>
+
       </BrowserRouter>
     </>
   )
