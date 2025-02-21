b43f4682a1cecfdf3138c6c84526dea3190ff760
rahul prajapati <rahulprajapati2930@gmail.com>
2025-02-21 16:40:18 +0530

    vx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from './Pages/Home/Home'
 import About from './Components/About'
 import OurMenu from './Components/OurMenu'
